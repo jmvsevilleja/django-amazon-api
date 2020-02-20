8eda9e40a390875a15496bd1dc84a1c46ad7f16d
Jess <jmvsevilleja@gmail.com>
2020-02-20 08:23:37 +0800

    Added sorting table

--- resources/js/components/Orders.js
+++ resources/js/components/Orders.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 // import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -8,8 +8,9 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
-import Grid from '@material-ui/core/Grid';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 
+import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import FilterListIcon from '@material-ui/icons/FilterList';
@@ -22,6 +23,106 @@ import { useState, useEffect } from 'react'
 // function preventDefault(event) {
 //   event.preventDefault();
 // }
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+const headCells = [
+  { id: 'order_id', numeric: false, disablePadding: false, label: 'Order ID' },
+  { id: 'settlement_id', numeric: true, disablePadding: false, label: 'Settlement ID' },
+  { id: 'sku', numeric: false, disablePadding: false, label: 'SKU' },
+  { id: 'description', numeric: false, disablePadding: false, label: 'Description' },
+  { id: 'quantity', numeric: true, disablePadding: false, label: 'Quantity' },
+  { id: 'product_sales', numeric: true, disablePadding: false, label: 'Product Sales' },
+];
+
+function EnhancedTableHead(props) {
+  const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+  const createSortHandler = property => event => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map(headCell => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'default'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  classes: PropTypes.object.isRequired,
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+const useToolbarStyles = makeStyles(theme => ({
+  root: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(1),
+  },
+  highlight:
+    theme.palette.type === 'light'
+      ? {
+        color: theme.palette.secondary.main,
+        backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+      }
+      : {
+        color: theme.palette.text.primary,
+        backgroundColor: theme.palette.secondary.dark,
+      },
+  title: {
+    flex: '1 1 100%',
+  },
+}));
 
 const useStyles = makeStyles(theme => ({
   seeMore: {
@@ -36,6 +137,27 @@ const useStyles = makeStyles(theme => ({
   filter: {
     width: 100,
   },
+  root: {
+    width: '100%',
+  },
+  paper: {
+    width: '100%',
+    marginBottom: theme.spacing(2),
+  },
+  table: {
+    minWidth: 750,
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 20,
+    width: 1,
+  },
 }));
 
 export default function Orders(props) {
@@ -44,11 +166,54 @@ export default function Orders(props) {
 
   const [page, setPage] = React.useState(0);
   const [data, setData] = useState([]);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
   const [sku, setSku] = React.useState('');
   const [from, setFrom] = React.useState('');
   const [to, setTo] = React.useState('');
+
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('calories');
+  const [selected, setSelected] = React.useState([]);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = event => {
+    if (event.target.checked) {
+      const newSelecteds = data.map(n => n.name);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const isSelected = name => selected.indexOf(name) !== -1;
+
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
 
   // Pagination
   const handleChangePage = (event, newPage) => {
@@ -131,7 +296,6 @@ export default function Orders(props) {
         </Grid>
 
         <Grid item xs >
-
           <Grid container alignItems="flex-start" justify="flex-end" direction="row" className={classes.grid}>
             <TextField
               id="sku"
@@ -178,38 +342,46 @@ export default function Orders(props) {
           </Grid>
         </Grid>
 
-
-
       </Grid>
-      <Table stickyHeader aria-label="sticky table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Order ID</TableCell>
-            <TableCell align="center">Settlement ID</TableCell>
-            <TableCell align="center">SKU</TableCell>
-            <TableCell align="left">Description</TableCell>
-            <TableCell align="right">Quantity</TableCell>
-            <TableCell align="right">Product Sales</TableCell>
-          </TableRow>
-        </TableHead>
+      <Table stickyHeader aria-label="sticky table" size='small'>
+        <EnhancedTableHead
+          classes={classes}
+          numSelected={selected.length}
+          order={order}
+          orderBy={orderBy}
+          onSelectAllClick={handleSelectAllClick}
+          onRequestSort={handleRequestSort}
+          rowCount={data.length}
+        />
         <TableBody>
-          {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
-            return (
-              <React.Fragment key={row.id}>
-                <TableRow >
-                  <TableCell component="th" scope="row">
-                    {row.order_id}
-                  </TableCell>
-                  <TableCell align="center">{row.settlement_id}</TableCell>
-                  <TableCell align="center">{row.sku}</TableCell>
-                  <TableCell align="left">{row.description}</TableCell>
-                  <TableCell align="right">{row.quantity}</TableCell>
-                  <TableCell align="right">{row.product_sales}</TableCell>
-                </TableRow>
-              </React.Fragment>
+          {stableSort(data, getComparator(order, orderBy))
+            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            .map((row, index) => {
+              const isItemSelected = isSelected(row.name);
+              const labelId = `enhanced-table-checkbox-${index}`;
 
-            );
-          })}
+              return (
+                <React.Fragment key={row.id}>
+                  <TableRow >
+                    <TableCell component="th" scope="row" title={row.date_time}>
+                      {row.order_id}
+                    </TableCell>
+                    <TableCell align="center">{row.settlement_id}</TableCell>
+                    <TableCell align="center">{row.sku}</TableCell>
+                    <TableCell align="left" >
+                      {row.description}
+                    </TableCell>
+                    <TableCell align="right">{row.quantity}</TableCell>
+                    <TableCell align="right">{row.product_sales}</TableCell>
+                  </TableRow>
+                </React.Fragment>
+              );
+            })}
+          {emptyRows > 0 && (
+            <TableRow style={{ height: (33) * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
         </TableBody>
       </Table>
 
